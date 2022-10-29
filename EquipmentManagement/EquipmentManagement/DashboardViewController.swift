f31b9124696cd33f1ee838a259620367a10108d7
Ashish <ashish@northeastern.edu>
2022-10-29 15:00:30 -0700

    Checking in code for adding UI Elements in the Dashboard VC

--- EquipmentManagement/EquipmentManagement/DashboardViewController.swift
+++ EquipmentManagement/EquipmentManagement/DashboardViewController.swift
@@ -10,24 +10,43 @@ import FirebaseAuth
 
 class DashboardViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtSerialNumber: UITextField!
+    @IBOutlet weak var lblEquipmentName: UILabel!
+    @IBOutlet weak var lblMakeName: UILabel!
+    @IBOutlet weak var lblModelName: UILabel!
+    @IBOutlet weak var lblRatingName: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func scanQRCodeAction(_ sender: Any) {
+    }
+    
+    @IBAction func submitAction(_ sender: Any) {
+    }
+    
+    @IBAction func viewHistoryAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func performMaintainanceAction(_ sender: Any) {
+    }
+    
+    
     @IBAction func logoutAction(_ sender: Any) {
-        
         let keychain = KeyChainService().keyChain
         keychain.clear()
-        
+
         do {
             try Auth.auth().signOut()
         }
         catch{
             print("Unable to logout")
         }
-        
+
         self.navigationController?.popViewController(animated: true)
-        
-        
     }
 }
